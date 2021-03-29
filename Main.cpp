3bed79e24a63e2d21264b032f594b8cbc083e118
Sohail Gsais <gsais.sohail9@gmail.com>
2021-03-29 18:58:17 +0100

    read file to datastructure

--- Main.cpp
+++ Main.cpp
@@ -2,24 +2,46 @@
 #include "HashTable.hpp"
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 #include <list>
-
+#include <ctype.h>
+/**
+ * @brief Main Program Runner
+ * TODOs : DEAL WITH COLLISIONs
+ * !FIX  : FILE HAS ISSUE AT LINE  396 
+ *
+ */
 
 int main(int argc, char const *argv[])
 {
     HashTabel h;
 
-    
-    //Deep Reinforcement Learning Hands-On	Maxim Lapan	9781788839303	7	
+    //Deep Reinforcement Learning Hands-On	Maxim Lapan	9781788839303	7
+    int lineCounter = 0;
+    std::ifstream datafile(argv[1]);
+    std::string data;
+    std::vector<std::string> tokens;
     std::vector<std::string> authors;
-    authors.push_back("James");
-    authors.push_back("TOM");
+    
+    while (getline(datafile, data))
+    {
+        // Output the text from the file
+        lineCounter++;
+        std::stringstream ss(data);
 
-    Book b = Book("Deep Reinforcement Learning Hands-On",authors,9781788839303,7);
-    Book b2 = Book("Sohail",authors,123,2);
-    h.insert(b);
-    h.insert(b2);
+        std::string title, author, ISBN, Q;
 
-    std::cout <<  h.search("Deep Reinforcement Learning Hands-On")<< std::endl;
+        std::getline(ss, title, '\t');
+        std::getline(ss, author, '\t');
+        std::getline(ss, ISBN, '\t');
+        std::getline(ss, Q, '\t');
+
+        authors.push_back(author);
+        Book b = Book(title, authors, std::stoul(ISBN), std::stoi(Q));
+        h.insert(b);
+    }
+
+    std::cout << h.search("Graph Databases") << std::endl;
     return 0;
 }
